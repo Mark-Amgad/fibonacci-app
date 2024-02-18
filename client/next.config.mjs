8aa11427cceb36f53c5dbb589325096efcdeea1e
Mark-Amgad <markamgad5@gmail.com>
2024-02-18 23:00:15 +0200

    - dockerize client and serve it using nginx - docker-compose for production

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "out",
+  output: "export",
+  //distDir: "out",
 };
 
 export default nextConfig;
